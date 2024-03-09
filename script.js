e326429077a922a993102a85673db737051fef05
Debora Lais <deboralaisnm@gmail.com>
2024-03-09 02:36:24 -0300

    html feito e começo do script

---
+++ script.js
@@ -0,0 +1,9 @@
+const modal = document.querySelector('.modal-container');
+const tbody = document.querySelector('tbody');
+const sNome = document.querySelector('#m-nome');
+const sEmail = document.querySelector('#m-email');
+const sCelular = document.querySelector('#m-celular');
+const sCidade = document.querySelector('#m-cidade');
+const sFuncao = document.querySelector('#m-funcao');
+const btnSalvar = document.querySelector('#btnSalvar');
+
